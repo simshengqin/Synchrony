982bee1e7c934b97c4bf9bb76c06248b23b491a3
simshengqin <54699127+simshengqin@users.noreply.github.com>
2021-06-26 19:40:12 +0800

    Fix importing of bootstrap and added a basic modal

--- src/app/shared/components/service-form/service-form.component.ts
+++ src/app/shared/components/service-form/service-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ModalDismissReasons, NgbModal} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-service-form',
@@ -7,8 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServiceFormComponent implements OnInit {
 
-  constructor() { }
+  title = 'appBootstrap';
+  closeResult: string;
 
+  constructor(private modalService: NgbModal) {}
+
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
+  }
   ngOnInit(): void {
   }
 
