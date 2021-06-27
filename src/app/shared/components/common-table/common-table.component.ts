d93697b7c63830c642eec23b595f8a8e5869f305
simshengqin <54699127+simshengqin@users.noreply.github.com>
2021-06-27 20:26:17 +0800

    Display the assignments data in a table format

--- src/app/shared/components/common-table/common-table.component.ts
+++ src/app/shared/components/common-table/common-table.component.ts
@@ -13,6 +13,28 @@ import {AssignmentService} from '../../../core/services/assignment.service';
   styleUrls: ['./common-table.component.scss']
 })
 export class CommonTableComponent implements OnInit {
+  assignmentNameColumn = TableColumn.assignment_name;
+  assignmentDueDatetimeColumn = TableColumn.assignment_due_datetime;
+  assignmentStudentColumn = TableColumn.assignment_student;
+  assignmentStatusColumn = TableColumn.assignment_status;
+  assignmentFeedbackDatetimeColumn = TableColumn.assignment_feedback_datetime;
+  assignmentInstructorColumn = TableColumn.assignment_instructor;
+  freelancerUsernameColumn = TableColumn.freelancer_username;
+  freelancerSchoolColumn = TableColumn.freelancer_school;
+  freelancerGroupColumn = TableColumn.freelancer_group;
+  wageCreatedDatetimeColumn = TableColumn.wage_created_datetime;
+  wageHoursColumn = TableColumn.wage_hours;
+  accountUsernameColumn = TableColumn.account_username;
+  actionsColumn = TableColumn.actions;
+  assignmentEditAction = TableAction.assignment_edit;
+  assignmentDeleteAction = TableAction.assignment_delete;
+  assignmentMarkAction = TableAction.assignment_mark;
+  assignmentSubmitAction = TableAction.assignment_submit;
+  assignmentResubmitAction = TableAction.assignment_resubmit;
+  assignmentFeedbackAction = TableAction.assignment_feedback;
+  assignmentStudentAction = TableAction.assignment_student;
+  freelancerHoursAction = TableAction.freelancer_hours;
+  accountDeleteAction = TableAction.account_delete;
   @Input() tableActions?: Array<TableAction>;
   @Input() tableColumns?: Array<TableColumn>;
   @ViewChild(ConfirmModalComponent) confirmModalComponent: ConfirmModalComponent;
@@ -30,5 +52,11 @@ export class CommonTableComponent implements OnInit {
   }
   onCloseModal(response: string) {
     console.log(response);
+  }
+
+  arrayContains(assignmentName: string) {
+    const tableColumn = TableColumn.assignment_name;
+    return this.tableColumns.includes(tableColumn);
+
   }
 }
