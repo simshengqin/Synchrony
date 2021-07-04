f235762bbe050336973ba5bd084e5386576b9f9f
simshengqin <54699127+simshengqin@users.noreply.github.com>
2021-07-04 22:37:44 +0800

    Fix account delete button

--- src/app/core/models/TableAction.ts
+++ src/app/core/models/TableAction.ts
@@ -1,11 +1,11 @@
 export enum TableAction {
   assignment_edit = 'Edit',
-  assignment_delete = 'Delete',
+  assignment_delete = 'assignment_delete',
   assignment_mark = 'Mark' ,
   assignment_submit = 'Submit',
   assignment_resubmit = 'resubmit',
   assignment_student_feedback = 'View_feedback_student',
   assignment_instructor_feedback = 'View_feedback_instructor',
   freelancer_hours = 'view_hours',
-  account_delete = 'Delete'
+  account_delete = 'account_delete'
 }
